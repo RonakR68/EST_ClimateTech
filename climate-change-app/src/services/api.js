3eb98b88f64b3853abf96a63342d7e96b9235502
Ronak <redkarronak@gmail.com>
2024-11-16 02:51:49 +0530

    deploy changes

--- climate-change-app/src/services/api.js
+++ climate-change-app/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL+'/api/climate-changes';
 console.log(API_URL);
 export const fetchClimateChanges = async () => {
-    return await axios.get(`${API_URL}/api/climate-changes`);
+    return await axios.get(`${API_URL}`);
 };
 
 export const fetchClimateChangeById = async (id) => {
